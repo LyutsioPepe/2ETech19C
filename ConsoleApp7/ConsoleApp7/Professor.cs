9fcfb4c7d13cd1453a6658687016f3aec16bb7b1
27-AW 2E <114576894+LyutsioPepe@users.noreply.github.com>
2022-11-16 12:18:53 +0100

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Professor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Professor
+    {   
+        public int salary;
+        protected int staffNumber;
+        private int yearsOfService;
+        public int numbersOfClasses;
+        public Student supervises;
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a1 = new Auto();
+            a1.model = "Opel";
+            a1.marka = "Astra";
+            a1.jedz();
+        }
+    }
+}
